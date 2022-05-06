37f613f7106a49ea7e1177ab7bb3cfb7c5ef49ad
Blacklion14 <rkush214@gmail.com>
2022-05-06 21:00:37 +0530

    Initial Commit

---
+++ Section 1 - Natural Language Processing/Python/natural_language_processing.py
@@ -0,0 +1,85 @@
+# Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Impporting Dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv' , delimiter= '\t' , quoting = 3)
+
+#Cleaning the texts
+import re 
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0 , 1000):
+    review = re.sub('[^a-zA-Z ]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    all_stopwords.remove('not')
+    review = [ps.stem(word) for word in review if not word in set(all_stopwords)]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+#Creating BAG of words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+x = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,-1].values
+    
+#Spliting Data Into Training And Test Set
+from sklearn.model_selection import train_test_split
+x_train , x_test , y_train , y_test = train_test_split(x, y, test_size = 0.25 , random_state = 0)
+
+#Training model on data set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(x_train , y_train)
+
+#Predicting the new result
+y_pred = classifier.predict(x_test)  
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#Making the confusion metrics
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)  
+    
+#Predicting accuracy score
+from sklearn.metrics import accuracy_score  
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)    
+                        
+##Predicting if a single review is positive or negative
+#Positive review
+new_review = 'I love this restaurant so much'
+new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+new_review = new_review.lower()
+new_review = new_review.split()
+ps = PorterStemmer()
+all_stopwords = stopwords.words('english')
+all_stopwords.remove('not')
+new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+new_review = ' '.join(new_review)
+new_corpus = [new_review]
+new_X_test = cv.transform(new_corpus).toarray()
+new_y_pred = classifier.predict(new_X_test)
+print(new_y_pred)
+
+#Negative review
+new_review = 'I hate this restaurant so much'
+new_review = re.sub('[^a-zA-Z]', ' ', new_review)
+new_review = new_review.lower()
+new_review = new_review.split()
+ps = PorterStemmer()
+all_stopwords = stopwords.words('english')
+all_stopwords.remove('not')
+new_review = [ps.stem(word) for word in new_review if not word in set(all_stopwords)]
+new_review = ' '.join(new_review)
+new_corpus = [new_review]
+new_X_test = cv.transform(new_corpus).toarray()
+new_y_pred = classifier.predict(new_X_test)
+print(new_y_pred)
